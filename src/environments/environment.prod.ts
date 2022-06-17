d62343a5ee6394b04e29ca491dcb150c9e760e6d
arvind-tagline <arvind.tagline@gmail.com>
2022-06-17 18:02:09 +0530

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  FIRBASE_API_KEY: 'AIzaSyA5qu2zwqnox56d2sXfVfM - B2oP6KIwBAE',
+  apiEndPoint: 'https://nodejsexamination.herokuapp.com'
 };
