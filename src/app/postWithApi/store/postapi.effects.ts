47b204b5fb371dfd9b303aeb8cdcf83b12b9f2e3
arvind-tagline <arvind.tagline@gmail.com>
2022-06-22 14:13:24 +0530

    stop unwanted api call

--- src/app/postWithApi/store/postapi.effects.ts
+++ src/app/postWithApi/store/postapi.effects.ts
@@ -2,22 +2,34 @@ import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { PostapiService } from "src/app/service/postapi.service";
 import { addPostApi, addPostApiSuccess, deletePost, deletePostSuccess, loadPostApi, loadPostApiSuccess, updatePost, updatePostSuccess } from "./postapi.actions";
-import { map, exhaustMap, tap, mergeMap, switchMap } from "rxjs";
+import { map, exhaustMap, tap, mergeMap, switchMap, withLatestFrom, of } from "rxjs";
 import { Update } from "@ngrx/entity";
 import { Posts } from "src/app/models/posts.model";
+import { Store } from "@ngrx/store";
+import { AppStates } from "src/app/store/app.state";
+import { getAllApiPost } from "./postapi.selector";
+import { dummyAction } from "src/app/auth/state/auth.actions";
 
 @Injectable()
 export class PostApiEffects {
 
-    constructor(private postApiService: PostapiService, private actions$: Actions) { }
+    constructor(private postApiService: PostapiService, private actions$: Actions, private store: Store<AppStates>) { }
 
     loadPostApi$ = createEffect(() => {
-        return this.actions$.pipe(ofType(loadPostApi), mergeMap((action) => {
-            return this.postApiService.getPostData().pipe(map((data) => {
-                console.log('data :>> ', data);
-                return loadPostApiSuccess({ postApi: data })
-            }))
-        }))
+        return this.actions$.pipe(
+            ofType(loadPostApi),
+            withLatestFrom(this.store.select(getAllApiPost)),
+            mergeMap(([action, posts]) => {
+                if (!posts.length) {
+                    return this.postApiService.getPostData().pipe(
+                        map((data) => {
+                            return loadPostApiSuccess({ postApi: data })
+                        })
+                    );
+                }
+                return of(dummyAction());
+            })
+        );
     });
 
     addPostApi$ = createEffect(() => {
